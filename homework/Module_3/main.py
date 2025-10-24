898c53c83dafd07d82f005b92a1895fbcb2a3dba
Ian Too <ianktoo@gmail.com>
2025-10-24 08:23:47 -0700

    We can now transcribe!

--- homework/Module_3/main.py
+++ homework/Module_3/main.py
@@ -1,9 +1,10 @@
 print("Week 3 Assignment: Voice Agent Development 😀")
 
 import os
+import time
 
 # Import necessary libraries
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import FileResponse
 
 # transcribe audio
@@ -12,43 +13,60 @@ from utils.asr import transcribe_audio
 # upload dir
 UPLOAD_DIR = "uploads/temp"
 TRANSCR_DIR = "uploads/audio"
+TRANSCR_TEXT_DIR = "uploads/text"
 # make sure they exist
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(TRANSCR_DIR, exist_ok=True)
+os.makedirs(TRANSCR_TEXT_DIR, exist_ok=True)
 
 app = FastAPI()
 
 @app.post("/chat/")
-async def chat_endpoint(file: UploadFile = File(...)):
+async def chat_endpoint(request: Request, file: UploadFile = File(...)):
     """
     Accepts audio file and returns audio file.
     """
 
+    # check for only one file.
+    form = await request.form()
+    if len(form) > 1:
+        return {"error": "Only one audio file is allowed per request."}
+    
     # save the uploaded file
+    audio_bytes = await file.read()
+
     uploaded_file = os.path.join(UPLOAD_DIR, file.filename)
     with open(uploaded_file, "wb") as f:
         print("Saving uploaded file.🆗")
         f.write(await file.read())
 
-    # read the uploaded file
-    with open(uploaded_file, "rb") as f:
-        print("Reading the uploaded file.")
-        audio_bytes = file.read()
+    print("Reading the uploaded file.")
 
     # TODO: ASR → LLM → TTS
 
-    # asr
-    some_text = transcribe_audio(audio_bytes=audio_bytes)
-    print(f"👌: {some_text}")
+    # transcribe
+    some_text = transcribe_audio(audio_bytes=audio_bytes, original_filename=file.filename)
+    print(f"\n👌: {some_text}\n")
+
+  
+    # Save transcription to a text file using timestamp
+    timestamp = time.strftime("%Y%m%d-%H%M%S")  # e.g., 20251024-075230
+        
+    # Save transcription to a text file
+    transcript_path = os.path.join(TRANSCR_TEXT_DIR, f"{timestamp}_{os.path.splitext(file.filename)[0]}_transcript.txt")
+    with open(transcript_path, "w") as transcript_file:
+        transcript_file.write(some_text)
+
+
 
     # file path 
     # # TODO: Return the actual file.
     # This is a placeholder for testing purposes
-    file_path = "test_data/audio/sample-0.mp3"
+    
    
     
     return FileResponse(
-        path=file_path,
+        path=uploaded_file, # use the uploaded file as placeholder
         media_type="audio/mpeg",
         filename="test.mp3"
     )
--- homework/Module_3/utils/asr.py
+++ homework/Module_3/utils/asr.py
@@ -1,2 +1,22 @@
-def transcribe_audio(audio_bytes):
-    return "something was transcribed. This is not an actual transcription. It is for testing purposes only."
+import os
+import tempfile
+import whisper
+
+# load the asr model
+asr_model = whisper.load_model("tiny")
+
+def transcribe_audio(audio_bytes, original_filename):
+    """
+    Accept an audio bytes
+    Write it to a temporary file
+    transcribe it
+    """
+    ext = os.path.splitext(original_filename)[1]
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(suffix=ext, delete=True) as temp_audio:
+        temp_audio.write(audio_bytes)
+        temp_audio.flush()  # Ensure data is written
+
+        # Transcribe using Whisper
+        result = asr_model.transcribe(temp_audio.name)
+        return result.get("text", "")
